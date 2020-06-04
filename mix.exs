e2224caaf8c009a8f1e79474351d7ee640939d5d
Andrea Leopardi <an.leopardi@gmail.com>
2020-06-04 08:55:51 +0200

    Update Broadway requirement to ~> 0.6.0

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule BroadwayRabbitMQ.MixProject do
 
   defp deps do
     [
-      {:broadway, "0.6.0"},
+      {:broadway, "~> 0.6.0"},
       {:amqp, "~> 1.3"},
       {:ex_doc, ">= 0.19.0", only: :docs}
     ]
