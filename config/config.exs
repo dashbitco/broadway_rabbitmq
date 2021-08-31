b0efe69b5ddcf2b17c0906542897552dc2b1d5ce
Dominic Morneau <dmorneau@gmail.com>
2021-08-31 22:46:04 +0900

    Fix support for older Elixir

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
-if :ok == Application.ensure_loaded(:lager) do
+# ensure_loaded was added in Elixir 1.10
+if Application.load(:lager) in [:ok, {:error, {:already_loaded, :lager}}] do
   config :lager,
     error_logger_redirect: false,
     handlers: [level: :critical]
